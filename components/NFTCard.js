72aae5ec170bcef8357b95426e5ddd88c9da29d1
Luciferxgkp <gouravbksc1@gmail.com>
2023-01-08 00:58:07 +0530

    added details page

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,6 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Image } from "react-native";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { CircleButton, RectangleButton } from "./Button";
+import { EthPrice, NFTTitle, SubInfo } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -20,13 +22,37 @@ const NFTCard = ({ data }) => {
           resizeMode="cover"
           style={{
             width: "100%",
-            width: "100%",
+            height: "100%",
             borderTopLeftRadius: SIZES.font,
             borderTopRightRadius: SIZES.font,
           }}
         />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFTCard</Text>
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectangleButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- components/index.js
+++ components/index.js
@@ -1,5 +1,17 @@
+import { CircleButton, RectangleButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
 import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
-
-export { HomeHeader, FocusedStatusBar, NFTCard };
+import { SubInfo } from "./SubInfo";
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectangleButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
