93c08c0b824e3940837ed4591b6d3d5fd9c77d15
Luciferxgkp <gouravbksc1@gmail.com>
2023-01-07 22:20:08 +0530

    added NFT Page

---
+++ components/NFTCard.js
@@ -0,0 +1,34 @@
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image } from "react-native";
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            width: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+      </View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+};
+
+export default NFTCard;
