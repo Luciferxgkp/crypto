c39a5c8b613f36477f2e34b9544d368facace899
Luciferxgkp <gouravbksc1@gmail.com>
2023-01-08 15:01:01 +0530

    added search functionallity

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import { FlatList, SafeAreaView, Text, View } from "react-native";
 import { FocusedStatusBar, NFTCard, HomeHeader } from "../components";
 import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+    if (!filteredData.length) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
           <View
             style={{
